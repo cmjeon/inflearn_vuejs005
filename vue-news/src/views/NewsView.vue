ac0c832b0704749d73ffc343e51a35f9873507b1
cmj <chmin82@gmail.com>
2020-10-15 21:43:17 +0900

    API 구현 - 1

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>news</div>
+  <div>
+    <div v-for="user in users">{{ user.title }}</div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-
+  data() {
+    return{
+      users:[]
+    }
+  },
+  created() {
+    var vm = this;
+    axios.get('https://api.hnpwa.com/v0/news/1.json')
+    .then((response) => {
+      // this.users = response.data;
+      console.log(response);
+      vm.users = response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  },
 }
 </script>
 
