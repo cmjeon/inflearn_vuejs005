09ae38217bb5530872ad9d0570967c456e0a4512
cmj <chmin82@gmail.com>
2020-10-17 00:56:40 +0900

    스토어 구현 - 7

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,61 +1,23 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { fetchNewsList } from '../api/index.js';
-import { fetchAskList } from '../api/index.js';
-import { fetchJobsList } from '../api/index.js';
+
+import mutations from './mutations';
+import actions from './actions';
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-  actions: {
-    FETCH_NEWS(context) {
-      fetchNewsList()
-        .then(response  => {
-          context.commit('SET_NEWS', response.data);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    },
-    FETCH_ASK({ commit }) {
-      fetchAskList()
-        .then(({ data })  => {
-          commit('SET_ASK', data);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    },
-    FETCH_JOBS({ commit }) {
-      fetchJobsList()
-        .then(({ data }) => {
-          commit('SET_JOBS', data);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
+  state: {
+    news: [],
+    ask: [],
+    jobs: []
   },
   getters: {
     fetchedAsk(state) {
       return state.ask;
     }
   },
-  mutations: {
-    SET_NEWS(state, news) {
-      state.news = news;
-    },
-    SET_ASK(state, ask) {
-      state.ask = ask;
-    },
-    SET_JOBS(state, jobs) {
-      state.jobs = jobs;
-    }
-  },
-  state: {
-    news: [],
-    ask: [],
-    jobs: []
-  },
+  mutations,
+  actions,
 });
 
