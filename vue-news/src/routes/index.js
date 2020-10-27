b714a48b67ba0e9e638fae91af807ad665d36a86
cmj <chmin82@gmail.com>
2020-10-27 21:04:20 +0900

    리팩토링 3 - Mixin과 하이 오더 컴포넌트 - 6

--- vue-news/src/routes/index.js
+++ vue-news/src/routes/index.js
@@ -1,12 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import NewsView from '../views/NewsView.vue';
-import AskView from '../views/AskView.vue';
-import JobsView from '../views/JobsView.vue';
+// import NewsView from '../views/NewsView.vue';
+// import AskView from '../views/AskView.vue';
+// import JobsView from '../views/JobsView.vue';
 import UserView from '../views/UserView.vue';
 import ItemView from '../views/ItemView.vue';
-import CraeteListView from  '../views/CreateListView.js';
-import createListView from '../views/CreateListView.js';
+import craeteListView from  '../views/CreateListView.js';
 
 Vue.use(VueRouter);
 
@@ -21,19 +20,19 @@ export const router = new VueRouter({
       path: '/news',
       name: 'news',
       // component: NewsView,
-      component: createListView('NewsView'),
+      component: craeteListView('NewsView'),
     },
     {
       path: '/ask',
       name: 'ask',
       // component: AskView,
-      component: createListView('AskView'),
+      component: craeteListView('AskView'),
     },
     {
       path: '/jobs',
       name: 'jobs',
       // component: JobsView,
-      component: createListView('JobsView'),
+      component: craeteListView('JobsView'),
     },
     {
       path: '/user/:id',
--- vue-news/src/views/CreateListView.js
+++ vue-news/src/views/CreateListView.js
@@ -4,11 +4,10 @@ import bus from '../utils/bus.js';
 export default function createListView(name) {
   return {
     // 재사용할 인스턴스(컴포넌트) 옵션들이 들어갈 자리
-    name: name,
+    name,
     created() {
       bus.$emit('start:spinner');
-      setTimeout(() =>{
-        this.$store.dispatch('FETCH_LIST', this.$route.name)
+      this.$store.dispatch('FETCH_LIST', this.$route.name)
         .then(() => {
           console.log('fetched');
           bus.$emit('end:spinner');
@@ -16,7 +15,6 @@ export default function createListView(name) {
         .catch((error) => {
           console.log(error)
         });
-      }, 1000);
     },
     render(createElement) {
       return createElement(ListView);
