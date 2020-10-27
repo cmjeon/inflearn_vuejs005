6a10c704a5ff1bef4dfe995a2bd253f427b7c69e
cmj <chmin82@gmail.com>
2020-10-27 11:31:51 +0900

    리팩토링 1 - 리스트 아이템 컴포넌트 공통화 - 4

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,71 +1,18 @@
 <template>
   <div>
-    <ul class="news-list">
-      <li v-for="item in this.$store.state.news" class="post">
-        <!-- 포인트 영역 -->
-        <div class="points">
-          {{ item.points }}
-        </div>
-        <!-- 기타 영역 정보 -->
-        <div>
-          <p class="news-title">
-            <a v-bind:href="item.url">
-              {{ item.title }}
-            </a>
-          </p>
-          <small class="link-text">
-            {{ item.time_ago }} by
-            <router-link v-bind:to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link>
-          </small>
-        </div>
-      </li>
-    </ul>
-    <!-- <p v-for="item in this.$store.state.news">
-      <router-link v-bind:to="`/item/${item.id}`">
-        {{ item.title}}
-      </router-link>
-      <small>
-        {{ item.time_ago }} by 
-        <router-link v-bind:to="`/user/${item.user}`">
-          {{ item.user }}
-        </router-link>
-      </small>
-    </p> -->
+    <list-item></list-item>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from '../api/index.js';
+import ListItem from '../components/ListItem.vue';
+
 export default {
-  created() {
-    this.$store.dispatch('FETCH_NEWS');
-  },
+  components: {
+    ListItem,
+  }
 }
 </script>
 
-<style scoped>
-.news-list {
-  margin:0;
-  padding:0;
-}
-.post {
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-.points{
-  width: 80px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #42b883;
-}
-.news-title {
-  margin:0;
-}
-.link-text {
-  color: #828282;
-}
+<style>
 </style>
