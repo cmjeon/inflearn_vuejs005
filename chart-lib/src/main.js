856f85e1cd836a948d9abf503a0521a665db86ab
cmj <chmin82@gmail.com>
2020-11-05 15:53:42 +0900

    외부 라이브러리 모듈화 방법(차트) - 7

--- chart-lib/src/main.js
+++ chart-lib/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ChartPlugin from './plugins/ChartPlugin.js';
 
 Vue.config.productionTip = false
 
+Vue.use(ChartPlugin);
+
 new Vue({
+  ChartPlugin,
   render: h => h(App),
 }).$mount('#app')
